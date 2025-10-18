c5d0856107d8fe17338328efac79478b7a12f10b
David Almeida <davidalmeida0147@gmail.com>
2025-10-17 22:47:33 -0300

    refactor: add user hash to user service

--- API/src/services/userService.ts
+++ API/src/services/userService.ts
@@ -1,10 +1,14 @@
+import { PasswordHelper } from "../helper/password.helper";
 import { CreateUserDTO } from "../interfaces/user.interface";
 import { UserRepository } from "../repositories/userRepository";
+import { UserValidator } from "../validator/user.validator";
 
 export class UserService {
     constructor(private readonly userRepository: UserRepository) {}
 
     create = async(userDTO: CreateUserDTO) => {
-        return await this.userRepository.create(userDTO);
+        UserValidator.validateCreate(userDTO);
+        const hashPassword = await PasswordHelper.hash(userDTO.password);
+        return await this.userRepository.create({...userDTO, password: hashPassword});
     }
 }
