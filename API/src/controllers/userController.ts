4ef37311e9cf96fcf7e4038828a59c709d6a1228
David Almeida <davidalmeida0147@gmail.com>
2025-10-18 10:35:47 -0300

    refactor: adjust user methods

--- API/src/controllers/userController.ts
+++ API/src/controllers/userController.ts
@@ -1,11 +1,11 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { UserService } from "../services/userService";
-import { CreateUserDTO } from "../interfaces/user.interface";
+import { CreateUserForm } from "../forms/user.form";
 export class UserController {
     
     constructor(private readonly userService: UserService) {}
 
-    create = async(request: FastifyRequest<{Body: CreateUserDTO}>, reply: FastifyReply) => {
+    create = async(request: FastifyRequest<{Body: CreateUserForm}>, reply: FastifyReply) => {
         try{
             const {name, email, password} = request.body;
             const createdUser = await this.userService.create({name, email,password});
--- API/src/services/userService.ts
+++ API/src/services/userService.ts
@@ -1,12 +1,12 @@
 import { PasswordHelper } from "../helper/password.helper";
-import { CreateUserDTO } from "../interfaces/user.interface";
+import { CreateUserForm } from "../forms/user.form";
 import { UserRepository } from "../repositories/userRepository";
 import { UserValidator } from "../validator/user.validator";
 
 export class UserService {
     constructor(private readonly userRepository: UserRepository) {}
 
-    create = async(userDTO: CreateUserDTO) => {
+    create = async(userDTO: CreateUserForm) => {
         UserValidator.validateCreate(userDTO);
         const hashPassword = await PasswordHelper.hash(userDTO.password);
         return await this.userRepository.create({...userDTO, password: hashPassword});
