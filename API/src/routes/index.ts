69672477b7ccfbb96eee3f23cdca23d2d2ffb0bb
David Almeida <davidalmeida0147@gmail.com>
2025-10-18 10:33:10 -0300

    feat: add student route and methods

--- API/src/routes/index.ts
+++ API/src/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { userRoute } from "./user.route";
+import { studentRoute } from "./student.route";
 
 export async function Routes(app: FastifyInstance) {
-    app.register(userRoute, {prefix: '/user'})
+    app.register(userRoute, {prefix: '/user'});
+    app.register(studentRoute, {prefix: '/student'});
 }
---
+++ API/src/routes/student.route.ts
@@ -0,0 +1,16 @@
+import { FastifyInstance } from "fastify";
+import { StudentRepository } from "../repositories/studentRepository";
+import { StudentService } from "../services/studentService";
+import { StudentController } from "../controllers/studentController";
+
+
+export async function studentRoute(app: FastifyInstance) {
+    const db = app.db;
+    const studentRepository = new StudentRepository(db);
+    const studentService = new StudentService(studentRepository);
+    const studentController = new StudentController(studentService);
+
+    app.post('/', studentController.create);
+    app.get('/', studentController.getAll);
+    app.delete('/:id', studentController.delete)
+}
