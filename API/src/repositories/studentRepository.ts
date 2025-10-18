87ca39ff0992b84f5096eb3a946641bbffcc8b3c
David Almeida <davidalmeida0147@gmail.com>
2025-10-17 22:54:51 -0300

    refactor: implement base repository in student and user repository

--- API/src/repositories/studentRepository.ts
+++ API/src/repositories/studentRepository.ts
@@ -1,16 +1,9 @@
 import { DataSource, Repository } from "typeorm";
 import { Student } from "../db/entities/Student";
+import { BaseRepository } from "./base/BaseRepository";
 
-export class StudentRepository {
-    private readonly repository: Repository<Student>
-    
+export class StudentRepository extends BaseRepository<Student> {
     constructor(dataSource: DataSource) {
-        this.repository = dataSource.getRepository(Student);
+        super(dataSource, Student);
     }
-
-    async create(studentData: Partial<Student>) {
-        const student = this.repository.create(studentData);
-        return await this.repository.save(student);
-    }
-
 }
--- API/src/repositories/userRepository.ts
+++ API/src/repositories/userRepository.ts
@@ -1,16 +1,9 @@
 import { DataSource, Repository } from "typeorm";
 import { User } from "../db/entities/User";
+import { BaseRepository } from "./base/BaseRepository";
 
-export class UserRepository {
-    private readonly repository: Repository<User>
-
+export class UserRepository extends BaseRepository<User> {
     constructor(dataSource: DataSource) {
-        this.repository = dataSource.getRepository(User);
+        super(dataSource, User)
     }
-
-    async create(UserData: Partial<User>) {
-        const user = this.repository.create(UserData);
-        return await this.repository.save(user);
-    }
-
 }
