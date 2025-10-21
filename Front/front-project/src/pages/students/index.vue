0f42904ff4fb0c47979af2cfae4cef67d6461112
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 14:16:55 -0300

    refactor: update Students page with delete modal and base layout

--- Front/front-project/src/pages/students/index.vue
+++ Front/front-project/src/pages/students/index.vue
@@ -4,6 +4,9 @@ import { getStudents } from '@/api/services/StudentService'
 import router from '@/router'
 import type { PaginationForm } from '@/api/forms/PaginationForm'
 import type { StudentsResponse } from '@/api/responses/GetStudentsResponse'
+import StudentModal from '@/components/ui/StudentModal.vue'
+import { maskCPF } from '@/utils/MaskUtils'
+import BaseCard from '@/components/ui/BaseCard.vue'
 
 const page = ref(1)
 const search = ref('')
@@ -14,10 +17,10 @@ const headers: readonly {
   align?: 'start' | 'center' | 'end'
   sortable?: boolean
 }[] = [
-  { title: 'ID', key: 'id', align: 'start' },
   { title: 'Name', key: 'name', align: 'start' },
   { title: 'Email', key: 'email', align: 'start' },
-  { title: 'Student Registration', key: 'studentRegistration', align: 'end' },
+  { title: 'Student Registration', key: 'studentRegistration', align: 'start' },
+  { title: 'Document', key: 'document', align: 'end' },
   { title: 'Actions', key: 'actions', align: 'end', sortable: false },
 ]
 
@@ -28,20 +31,32 @@ const editStudent = async (id: number) => {
   router.push(`/students/${id}`);
 }
 
+const deleteStudent = (id: number, name: string) => {
+  if (!id) return;
+  if (!name) return;
+
+  deleteModal.value = true;
+  selectedStudentId.value = id;
+  selectedStudentName.value = name;
+}
+
+const deleteModal = ref(false);
+const selectedStudentId = ref<number | null>(null);
+const selectedStudentName = ref<string>('');
+
 
   const itemsPerPage = ref(5)
   const serverItems = ref<StudentsResponse[]>([]);
   const loading = ref(true)
   const totalItems = ref(0)
   const loadItems = async(options: any) => {
-    debugger
     loading.value = true;
     
     try {
       const page = options.page ?? 1;
       const limit = options.itemsPerPage ?? 10;
       let sortBy: { key: string; order: 'asc' | 'desc' }[] | undefined = undefined;
-
+      const searchTerm = search.value.trim() ?? ''; 
       if (options.sortBy && options.sortBy.length > 0) {
         sortBy = options.sortBy.map((key: string, index: number) => ({
           key,
@@ -51,8 +66,12 @@ const editStudent = async (id: number) => {
 
       const sortField = sortBy?.[0]?.key ?? '';
       const order = sortBy?.[0]?.order ?? 'asc';
-      const response = await getStudents({ page, limit, sortField, order});
-      serverItems.value = response.data;
+      const response = await getStudents({ search: searchTerm, page, limit, sortField, order});
+      const formattedResponse = response.data.map(item => ({
+          ...item,
+          document: maskCPF(item.document),
+        }));
+      serverItems.value = formattedResponse;
       totalItems.value = response.totalItems;
 
     } catch (error) {
@@ -72,72 +91,62 @@ const loadingItems = async () => {
 </script>
 
 <template>
-  <v-card flat class="pa-4 mt-4" color="surface">
-    <v-card-title class="d-flex align-center justify-space-between">
-      <div class="text-h5 font-weight-medium">Students</div>
-      <v-btn color="primary" prepend-icon="mdi-plus" rounded="lg" @click="addStudent()">
-        Add new student
-      </v-btn>
-    </v-card-title>
-
-    <div class="d-flex align-center justify-space-between search-wrapper">
+  <BaseCard>
+    <template #header>
+      <v-card-title class="d-flex align-center justify-space-between">
+        <div class="text-h4 font-weight-bold">📚 Students Overview</div>
+        <v-btn color="primary" prepend-icon="mdi-plus-circle" rounded="lg" @click="addStudent()">Add New Student</v-btn>
+      </v-card-title>
+    </template>
+    <div class="d-flex align-center justify-start search-wrapper mt-4">
       <v-text-field
         v-model="search"
-        density="comfortable"
-        variant="outlined"
         placeholder="Search student by name..."
-        hide-details
-        class="flex-grow-1 mr-2"
+        rounded dense variant="outlined"
         prepend-inner-icon="mdi-magnify"
-        rounded
-        dense
+        class="flex-grow-1 search-input mr-2"
+        hide-details
         @keyup.enter="loadItems"
       />
-      <v-btn
-        color="primary"
-        rounded
-        dense
-        elevation="2"
-        @click="loadItems"
-        class="search-btn"
-      >
-        Search
-      </v-btn>
+      <v-btn color="primary" rounded dense class="search-btn" @click="loadItems">Search</v-btn>
     </div>
-
     <v-data-table-server
       v-model:items-per-page="itemsPerPage"
       :headers="headers"
       :items="serverItems"
       :items-length="totalItems"
       :loading="loading"
-      item-value="name"
-      density="compact"
+      item-value="id"
       @update:options="loadItems"
-      class="elevation-1 rounded mt-4"
+      class="elevation-1 rounded-lg mt-4"
+      density="compact"
     >
       <template v-slot:loading>
         <div class="flex items-center justify-center h-40">
-          <v-progress-circular
-            indeterminate
-            color="primary"
-            size="25"
-          ></v-progress-circular>
+          <v-progress-circular indeterminate color="primary" size="25"></v-progress-circular>
           <span class="ml-2 text-gray-500">Loading...</span>
         </div>
       </template>
+      <template v-slot:no-data>
+        <div class="flex items-center justify-center h-40">
+          <span class="ml-2 text-gray-500">No students found</span>
+        </div>
+      </template>
       <template v-slot:item.actions="{ item }">
-          <div class="actions-container">
-            <v-btn icon size="medium" color="primary" @click="editStudent(item.id)" variant="text">
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-            <v-btn icon size="medium" @click="editStudent(item!.id)" color="error" variant="text">
-              <v-icon>mdi-delete</v-icon>
-            </v-btn>
-          </div>
+        <div class="actions-container">
+          <v-btn icon size="medium" color="primary" variant="text" @click="editStudent(item.id)"><v-icon>mdi-pencil</v-icon></v-btn>
+          <v-btn icon size="medium" color="error"   variant="text" @click="deleteStudent(item.id, item.name)"><v-icon>mdi-delete</v-icon></v-btn>
+        </div>
       </template>
     </v-data-table-server>
-  </v-card>
+
+    <StudentModal 
+      v-model="deleteModal" 
+      :id="selectedStudentId!" 
+      :name="selectedStudentName" 
+      @closed="loadingItems()"
+    />
+  </BaseCard>
 </template>
 
 <style scoped>
@@ -146,7 +155,7 @@ const loadingItems = async () => {
   border: 1px solid #ccc;
   border-radius: 8px;
   margin-top: 10px;
-  min-height: 75vh;
+  min-height: 65vh;
 }
 
 .py-6 { padding-top: 1.5rem; padding-bottom: 1.5rem; }
@@ -201,5 +210,4 @@ const loadingItems = async () => {
   font-size: 0.875rem;
   text-transform: none;
 }
-
 </style>
