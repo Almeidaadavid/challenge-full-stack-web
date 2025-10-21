b41842c3edf0037e255cb640f13b5121743c1e92
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 19:51:05 -0300

    feat: configure swagger

---
+++ API/src/configs/swagger.config.ts
@@ -0,0 +1,20 @@
+import { FastifyInstance } from "fastify";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+
+export async function SwaggerConfig(app: FastifyInstance) {
+  await app.register(swagger, {
+    openapi: {
+      openapi: "3.0.0",
+      info: {
+        title: "Graduation API",
+        description: "API for managing students in a graduation system",
+        version: "1.0.0",
+      },
+    },
+  });
+
+  await app.register(swaggerUI, {
+    routePrefix: "/docs",
+  });
+}
