0d792c26d5f4f732ed780e19f6adedd90d529f01
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 03:21:25 -0300

    feat: add created components in main page

--- Front/front-project/src/App.vue
+++ Front/front-project/src/App.vue
@@ -1,11 +1,31 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="application-layout">
+      <div v-if="showLayout">
+        <AppBarComponent @toggle-drawer="drawer = !drawer" />
+        <NavigationDrawerComponent v-model="drawer" :user="userRef" />
+      </div>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import AppBarComponent from './components/AppBarComponent.vue';
+import NavigationDrawerComponent from './components/NavigationDrawerComponent.vue';
+import { ref, computed } from 'vue'
+import { isLoggedIn, user } from '@/composable/authState';
+import { useRoute } from 'vue-router';
+  const drawer = ref(false);
+  const userRef = user;
+  const route = useRoute();
+  
+  const publicPages = ['/login', '/register', '/forgot-password'];
+  const showLayout = computed(() => isLoggedIn.value && !publicPages.includes(route.path));
 </script>
+
+<style scoped>
+  .application-layout {
+    background: #f9fafb;
+  }
+</style>
