fe8c4de44f58ddb8363d9e1a86fa7133d61fb037
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 03:25:03 -0300

    feat: create the student interface

---
+++ Front/front-project/src/types/student.interface.ts
@@ -0,0 +1,9 @@
+export interface Student {
+    id: number,
+    name: string,
+    email: string,
+    studentRegistration: string,
+    document: string,
+    course: string,
+    cellphone: string,
+}
