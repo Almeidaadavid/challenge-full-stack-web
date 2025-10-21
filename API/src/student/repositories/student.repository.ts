398021d32d7627dff77f4f465e7cfb38eebdc657
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 03:31:11 -0300

    feat: adjust the query to adapte students loader

--- API/src/student/repositories/student.repository.ts
+++ API/src/student/repositories/student.repository.ts
@@ -1,9 +1,42 @@
-import { DataSource, Repository } from "typeorm";
+import { DataSource, ILike, Repository } from "typeorm";
 import { Student } from "../entities/student.entity";
 import { BaseRepository } from "../../common/repositories/BaseRepository";
+import { PaginationDto } from "../../common/dtos/pagination.dto";
 
 export class StudentRepository extends BaseRepository<Student> {
     constructor(dataSource: DataSource) {
         super(dataSource, Student);
+    }
+
+     async findAllStudent(paginationDto: PaginationDto) {
+        const {search, page, limit, sortBy} = paginationDto;
+        const where: any = {};
+        if (search) {
+            where.name = ILike(`%${search}%`);
+        }
+
+        let order: any = {};
+        if (sortBy && sortBy.length > 0) {
+            const key = sortBy[0]?.key ?? '';
+            const direction = sortBy[0]?.order ?? 'asc';
+            if (key) {
+                order[key] = direction?.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+            }
+        }
+
+        const [data, total] = await this.repository.findAndCount({
+            where: where,
+            take: limit,
+            skip: (page - 1) * limit,
+            order,
+        });
+
+        return {
+            data,
+            page: +page,
+            limit: +limit,
+            totalItems: total,
+            totalPages: Math.ceil(total / limit),
+        };
     }
 }
