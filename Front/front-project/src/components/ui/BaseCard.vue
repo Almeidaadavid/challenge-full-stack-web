02d5f815028e9ca35aea4c04b96dc27d1405ade5
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 14:14:34 -0300

    feat: create base card component

---
+++ Front/front-project/src/components/ui/BaseCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid class="base-container pa-6">
+    <v-card outlined class="base-card pa-4 rounded-lg elevation-4" color="surface">
+      <slot name="header"></slot>
+      <slot></slot>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.base-container {
+  background-color: #f7f9fc;
+}
+
+.base-card {
+  color: white;
+}
+</style>
