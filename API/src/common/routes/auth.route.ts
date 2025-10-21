dfe0c392311bdd143a74d764ada64a9822957760
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 19:55:34 -0300

    docs: configure login route

--- API/src/common/routes/auth.route.ts
+++ API/src/common/routes/auth.route.ts
@@ -9,5 +9,42 @@ export async function authRoute(app: FastifyInstance) {
     const authService = new AuthService(userRepository);
     const authController = new AuthController(authService);
 
-    app.post('/', authController.login);
+    app.post('/', {
+        schema: {
+            description: 'User login',
+            tags: ['Auth'],
+            body: {
+                type: 'object',
+                required: ['email', 'password'],
+                properties: {
+                    email: { type: 'string', format: 'email'},
+                    password: { type: 'string' }
+                },
+            },
+            response: {
+                200: {
+                    type: 'object',
+                    properties: {
+                        token: { type: 'string' },
+                        user: { 
+                            type: 'object', 
+                            properties: {
+                                id: { type: 'string' },
+                                email: { type: 'string', format: 'email' },
+                                name: { type: 'string' }
+                            }
+                        }
+                    }
+                },
+                401: {
+                    type: 'object',
+                    properties: {
+                        message: {
+                            type: 'string'
+                        }
+                    }
+                }
+            }
+        }
+    }, authController.login);
 }
