0c24176847b685730687fbcbcb4a95c89b6c4707
David Almeida <davidalmeida0147@gmail.com>
2025-10-21 21:57:14 -0300

    docs: add swagger auth configuration

--- API/src/configs/swagger.config.ts
+++ API/src/configs/swagger.config.ts
@@ -3,18 +3,27 @@ import swagger from "@fastify/swagger";
 import swaggerUI from "@fastify/swagger-ui";
 
 export async function SwaggerConfig(app: FastifyInstance) {
-  await app.register(swagger, {
-    openapi: {
-      openapi: "3.0.0",
-      info: {
-        title: "Graduation API",
-        description: "API for managing students in a graduation system",
-        version: "1.0.0",
-      },
-    },
-  });
+    await app.register(swagger, {
+        openapi: {
+        openapi: "3.0.0",
+        info: {
+            title: "Graduation API",
+            description: "API for managing students in a graduation system",
+            version: "1.0.0",
+        },
+        components: {
+                securitySchemes: {
+                    bearerAuth: {
+                        type: 'http',
+                        scheme: 'bearer',
+                        bearerFormat: 'JWT'
+                    }
+                }
+            },
+        }
+    });
 
-  await app.register(swaggerUI, {
-    routePrefix: "/docs",
-  });
+    await app.register(swaggerUI, {
+        routePrefix: "/docs",
+    });
 }
